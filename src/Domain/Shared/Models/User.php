e11a6143782b571097ce5f5269dc41288637e463
Mohammad Javad Bakhshi <mjavad.bakhshi@gmail.com>
2023-08-22 19:25:03 +0330

    Initial commit / Apply DDD structure and  namespace and configuration

---
+++ src/Domain/Shared/Models/User.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Domain\Shared\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\LaravelData\WithData;
+
+use Domain\Shared\DataTransferObjects\UserData;
+use Database\Factories\Shared\UserFactory;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable, WithData;
+
+    protected $dataClass = UserData::class;
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+
+    public static function newFactory() {
+        return app(UserFactory::class);
+    }
+
+}
